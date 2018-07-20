bab9606e81089a57a6631dec2c0580b188f00ebd
TAROT\sasaki <sasaki@freemake.co.jp>
2018-07-20 12:43:57 +0900

    ソース一式

---
+++ HelloWPFApp/Comport.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.IO;
+
+namespace HelloWPFApp
+{
+    class Comport
+    {
+        string portNum = "";
+        int baudRate = 9600;
+        Parity parity = Parity.Even;
+        int dataBits = 7;
+        StopBits stopBits = StopBits.One;
+
+        private static SerialPort port = new SerialPort();
+
+        public void PortOpen()
+        {
+            //File.AppendAllText("C:/Users/sasaki.TAROT/Desktop/log.txt", "Start");
+            port.PortName = portNum;
+            port.BaudRate = baudRate;
+            port.Parity = parity;
+            port.DataBits = dataBits;
+            port.StopBits = stopBits;
+            port.DtrEnable = true;
+            port.Open();
+            port.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+        }
+
+        public void PortClose()
+        {
+            port.Close();
+            port.Dispose();
+        }
+
+        private static void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            port = (SerialPort)sender;
+            byte[] buf = new byte[1024];
+            int len = port.Read(buf, 0, 1024);
+            string s = Encoding.GetEncoding("Shift_JIS").GetString(buf, 0, len);
+            Console.WriteLine(s);
+            File.AppendAllText("C:/Users/sasaki.TAROT/Desktop/log.txt", s);
+        }
+
+    }
+}
---
+++ HelloWPFApp/Greeting.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace HelloWPFApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Comport comport = new Comport();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenComPort(object sender, RoutedEventArgs e)
+        {
+            comport.PortOpen();
+        }
+
+        private void CloseComPort(object sender, RoutedEventArgs e)
+        {
+            comport.PortClose();
+        }
+    }
+
+}
